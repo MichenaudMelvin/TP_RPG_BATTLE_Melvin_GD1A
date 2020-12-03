db2830f5c0a20f7e8ba1cc4348654c04a7e514fa
MichenaudMelvin <m.michenaud@rns.ecolescreatives.com>
2020-12-03 22:04:44 +0100

    Main program

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-var sort = document.getElementById("special");
+/*pv des perso*/
+var pv_perso_un = document.getElementById("pv_perso_un");
+var pv_perso_deux = document.getElementById("pv_perso_deux");
+var pv_perso_quatre = document.getElementById("pv_perso_trois");
+var pv_perso_quatre = document.getElementById("pv_perso_quatre");
 
-sort.addEventListerner("mouseenter",function(event)){
-	event.target.style.color = "purple"
+/*mana des perso*/
+var mana_perso_un = document.getElementById("mana_perso_un");
+var mana_perso_deux = document.getElementById("mana_perso_deux");
+var mana_perso_trois = document.getElementById("mana_perso_trois");
+var mana_perso_quatre = document.getElementById("mana_perso_quatre");
+
+/*pv des boss*/
+var pv_boss_un
+var pv_boss_deux
+var pv_boss_trois = document.getElementById("")
+
+function attaque() {
+  document.getElementById("attaque").innerHTML = "> attaque <";
 }
 
+function defense() {
+  document.getElementById("defense").innerHTML = "> defense <";
+}
+
+function special() {
+  document.getElementById("special").innerHTML = "> special <";
+}
