100f5783eca33ebc4e26079f4b455ab04bb975f1
MichenaudMelvin <m.michenaud@rns.ecolescreatives.com>
2020-12-05 10:44:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 /*pv des perso*/
-var Number(pv_perso_un) = document.getElementById("pv_perso_un");
-var Number(pv_perso_deux) = document.getElementById("pv_perso_deux");
-var Number(pv_perso_quatre) = document.getElementById("pv_perso_trois");
-var Number(pv_perso_quatre) = document.getElementById("pv_perso_quatre");
+var pv_perso_un = Number(document.getElementById("pv_perso_un"));
+var pv_perso_deux = Number(document.getElementById("pv_perso_deux"));
+var pv_perso_quatre = Number(document.getElementById("pv_perso_trois"));
+var pv_perso_quatre = Number(document.getElementById("pv_perso_quatre"));
+
+
 
 /*mana des perso*/
-var Number(mana_perso_un) = document.getElementById("mana_perso_un");
-var Number(mana_perso_deux) = document.getElementById("mana_perso_deux");
-var Number(mana_perso_trois) = document.getElementById("mana_perso_trois");
-var Number(mana_perso_quatre) = document.getElementById("mana_perso_quatre");
+var mana_perso_un = Number(document.getElementById("mana_perso_un"));
+var mana_perso_deux = Number(document.getElementById("mana_perso_deux"));
+var mana_perso_trois = Number(document.getElementById("mana_perso_trois"));
+var mana_perso_quatre = Number(document.getElementById("mana_perso_quatre"));
+
+console.log(typeof pv_perso_un);
+
 
 /*pv des boss*/
-var Number(pv_boss_un) = document.getElementById("pv_boss_un")
-var Number(pv_boss_deux) = document.getElementById("pv_boss_deux")
-var Number(pv_boss_trois) = document.getElementById("pv_boss_trois")
-
-
+var pv_boss_un = Number(document.getElementById("pv_boss_un"));
+var pv_boss_deux = Number(document.getElementById("pv_boss_deux"));
+var pv_boss_trois = Number(document.getElementById("pv_boss_trois"));
 
 function attaque() {
   document.getElementById("attaque").innerHTML = "> attaque <";
@@ -34,9 +37,4 @@ function testPerdu(pv_boss_un, pv_boss_deux, pv_boss_trois, pv_perso_quatre){
 		var lose = true;
 	return lose;
 	}
-}
-
-function attaque(pv_perso_un, pv_boss_deux, pv_boss_trois) {
-	document.getElementById("attaque").innerHTML = "> attaque <";
-
 }
