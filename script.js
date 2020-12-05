ebb9517518171a584886cf997b7d4a859c76e5eb
MichenaudMelvin <m.michenaud@rns.ecolescreatives.com>
2020-12-05 17:35:26 +0100

    Main program

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function attaque(min, max) {
   return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
-function defense() {
+function defense(){
   document.getElementById("defense").innerHTML = "> Defense <";
 }
 
-function special() {
+function special(){
   document.getElementById("special").innerHTML = "> Special <";
 }
 
@@ -21,7 +21,7 @@ function testPerdu(pv_boss_un, pv_boss_deux, pv_boss_trois, pv_perso_quatre){
 	}
 }
 
-function attaquePerso(bossChoisi) {
+function attaquePerso(bossChoisi){
 	document.getElementById("attaque").innerHTML = "> Attaque <";
 	var nombre_attaque = attaque(20,30);
 	bossChoisi = bossChoisi - nombre_attaque;
@@ -30,6 +30,11 @@ function attaquePerso(bossChoisi) {
 }
 
 //main program
+var perso_un = Number(document.getElementById("pv_perso_un"));
+var perso_deux = Number(document.getElementById("pv_perso_deux"));
+var perso_quatre = Number(document.getElementById("pv_perso_trois"));
+var perso_quatre = Number(document.getElementById("pv_perso_quatre"));
+
 //pv des perso
 var pv_perso_un = Number(document.getElementById("pv_perso_un"));
 var pv_perso_deux = Number(document.getElementById("pv_perso_deux"));
@@ -47,16 +52,18 @@ var pv_boss_un = Number(document.getElementById("pv_boss_un"));
 var pv_boss_deux = Number(document.getElementById("pv_boss_deux"));
 var pv_boss_trois = Number(document.getElementById("pv_boss_trois"));
 
+var win = false;
+var lose = false;
+
 /*
 Déroulé d'un tour :
 perso 1 :
 choisi un ennemi
 choisi une action
 perso n+1
-
-while (win != true or lose != true){
-	//Tour du perso 1
-
-}
-
 */
+
+while (win != true || lose != true){
+	document.getElementById("message_box").innerHTML = ("Tour de");
+	document.getElementById("perso_un").style.color = "red";
+}
